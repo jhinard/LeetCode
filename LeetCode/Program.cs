169b76abba848e0490c7fdae190f77bad4b5dbbf
Kalaghann <jeremyhinard@gmail.com>
2017-01-29 21:48:03 +0100

    First commit

---
+++ LeetCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using LeetCode.Problems;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new TwoSum().Execute();
+        }
+    }
+}
